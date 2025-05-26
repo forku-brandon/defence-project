5a4f45576ad5ff757435db9dcef41eb538420a8a
Forku Brandon <brandonforku@gmail.com>
2025-05-26 16:33:55 +0100

    Update app.js

--- wataxi/passenger/backend/app.js
+++ wataxi/passenger/backend/app.js
@@ -7,7 +7,6 @@ import {
     signOut,
     onAuthStateChanged 
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
